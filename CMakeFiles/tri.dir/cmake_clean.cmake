5527b7771ea0cc153a78ef4946bbf4bde4a97e52
ilyes-guy <ilyes.guy@gmail.com>
2023-01-30 21:47:03 +0100

    opengl_tutorial

---
+++ CMakeFiles/tri.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tri.dir/glad.c.o"
+  "CMakeFiles/tri.dir/glad.c.o.d"
+  "CMakeFiles/tri.dir/main.cpp.o"
+  "CMakeFiles/tri.dir/main.cpp.o.d"
+  "tri"
+  "tri.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/tri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
