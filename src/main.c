289aa1582f33e8776c6f8b4d835864af34574c51
noot-noot-pengu <random_dude_233@proton.me>
2023-06-28 23:12:54 +0100

    test

--- src/main.c
+++ src/main.c
@@ -1,26 +1,95 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <time.h>
+#define memory_size 30000
 
-int
-main() {
-    pid_t child_pid;
-    pid_t wait_result;
-    int stat_loc;
+unsigned char memory[memory_size];
+int pointer = 0;
 
-    child_pid = fork();
-        
-    // The child process
-    if (child_pid == 0) {
-        printf("### Child ###\nCurrent PID: %d and Child PID: %d\n",
-               getpid(), child_pid);
-        sleep(1); // Sleep for one second
-    } else {
-        wait_result = waitpid(child_pid, &stat_loc, WUNTRACED);
-        printf("### Parent ###\nCurrent PID: %d and Child PID: %d\n",
-               getpid(), child_pid);
+// function declaration
+int is_operation(char c);
+void operation(char c);
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+	if (argv[1] == NULL){
+		printf("Error: excpected file name argument\n");
+        return 0;
+	}
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == 0){
+        printf("Error: Could not open source file\n");
+        return 0;
     }
- 
-    return 0;
+
+
+    while(1) {
+        char c = fgetc(file);
+        if (feof(file))
+            break ;
+
+
+		int gg = is_operation(c);
+		if (gg) {
+			operation(c);
+		}
+        printf("%i", gg);
+
+	}
+
+    fclose(file);
+
+	return 0;
+}
+
+
+
+
+int is_operation(char c){
+	if (c == '+' || c == '-' || c == '<' || c == '>' || c == '[' || c == ']' || c == '.' || c == ','){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+void operation(char c){
+	switch (c) {
+		case '+':
+			memory[pointer]++;
+        	break;
+		case '-':
+			memory[pointer]--;
+        	break;
+		case '<':
+			if (pointer == 0){
+				pointer = memory_size-1;
+			}else{
+				pointer--;
+			}
+        	break;
+		case '>':
+			if (pointer == memory_size-1){
+				pointer = 0;
+			}else{
+				pointer++;
+			}
+        	break;
+		case '[':
+			
+        	break;
+		case ']':
+			
+        	break;
+		case '.':
+        	printf("%c", memory[pointer]);
+        	break;
+		case ',':
+			
+        	break;
+	}
 }
